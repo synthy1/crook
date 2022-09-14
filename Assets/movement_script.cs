9f45344f961bd3739d973caea7fe3f3443f7adb4
synthy1 <jackhopson69@gmail.com>
2022-09-14 19:45:24 +0100

    adding sprint

--- Assets/movement_script.cs
+++ Assets/movement_script.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class movement_script : MonoBehaviour
 {
     Rigidbody2D layerobject;
-   
-    public float speed = 100f;
+
+    public float stamina = 100f;
+    public float speed = 10f;
+    public float basespeed = 10f;
+    public float sprspeed = 20f;
+    public float jmpforce = 100f;
     public GameObject groundchecker;
     public LayerMask whatisground;
     public float jumpf = 100f;
-    bool isJumping = true;
-    bool isgrounded = true;
+    bool isJumping = false;
+    bool isgrounded = false;
+    bool issliding = false;
+    bool issprint = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,16 +29,47 @@ public class movement_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float movmentValueX = Input.GetAxis("Horizontal") * speed*Time.deltaTime;
+        float movmentValueX = Input.GetAxis("Horizontal") * speed;
 
         layerobject.velocity = new Vector2(movmentValueX, layerobject.velocity.y);
-        isgrounded = Physics2D.OverlapCircle(groundchecker.transform.position, 1.0f, whatisground);
+        isgrounded = Physics2D.OverlapCircle(groundchecker.transform.position, 0.1f, whatisground);
 
         if (Input.GetKeyDown(KeyCode.Space) && isgrounded == true)
         {
-            layerobject.AddForce(new Vector2(0f, 100f));
+            layerobject.AddForce(new Vector2(0f, jumpf));
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && stamina < 0f)
+        {
+            issprint = true;
+            
+        }
 
+        if (Input.GetKeyUp(KeyCode.LeftShift) || stamina >= 0f)
+        {
+            issprint = false;
+            
+        }
+
+        if (issprint == true)
+        {
+            speed = sprspeed;
+            
+        }
+        else if (issprint == false)
+        {
+            speed = basespeed;
+            
+        }
+        while (issprint == true)
+        {
+            stamina = stamina - 1f;
+        }
+
+        while (issprint == false)
+        {
+            stamina = stamina + 1f;
+        }
+        Debug.Log(stamina);
     }
 }
